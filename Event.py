de053bcad35f82bc136e52e3c1978079992bc656
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2021-09-27 20:17:20 -0400

    Added stats helper for q1

--- Event.py
+++ Event.py
@@ -3,6 +3,10 @@ from datetime import datetime
 
 class Event:
 
-    def __init__(self, type='Departure', time=datetime.now()):
+    def __init__(self,
+                 type='Departure',
+                 time=datetime.now(),
+                 length=1):
         self.type = type
         self.time = time
+        self.length = length
--- main.py
+++ main.py
@@ -3,18 +3,17 @@ from NumberGenerator import NumberGenerator
 from Event import Event
 from DiscreteEventSimulator import DiscreteEventSimulator
 
+
 def main():
     print("Hello World!")
-    print(NumberGenerator.poisson(0.1))
-    des = DiscreteEventSimulator(10)
-    event1 = Event()
-    time.sleep(5)
-    event2 = Event()
-    des.add_event(event1)
-    des.add_event(event2)
-    print(des.remove_event().time)
-    print(des.remove_event().time)
+    print(get_stats())
 
+
+def get_stats(num_trials=1000, lambd=75):
+    random_vars = [NumberGenerator.poisson(lambd) for _ in range(0, num_trials)]
+    mean = sum(random_vars) / num_trials
+    variance = sum([(var - mean)**2 for var in random_vars]) / num_trials
+    return (mean, variance)
 
 
 if __name__ == "__main__":
