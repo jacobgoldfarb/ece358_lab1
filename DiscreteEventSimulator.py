51e43b1fefce7a1d5bccfceab4be15f9b99dbf66
Jacob Goldfarb <jacobgoldfarb1997@gmail.com>
2021-10-09 18:48:02 -0400

    comment documentation

--- DiscreteEventSimulator.py
+++ DiscreteEventSimulator.py
@@ -11,28 +11,37 @@ class DiscreteEventSimulator:
         self.capacity = capacity
         self.simulation_time = simulation_time
 
+        # Double-ended queue to be updated as packets are enqueued and dequeued.
         self.packet_queue = deque()
-        self.proportion_lost = 0
+
+        # Event storage
         self.events = []
         self.observer_events = []
         self.dropped_events = []
+
+        # Metrics to be calculated by looping through observers.
+        self.proportion_lost = 0
         self.q_idle_proportions = 0
+        self.avg_q_size = 0
 
     def total_num_packets(self):
         return float(len(self.events) + len(self.dropped_events))
 
     def proportion_of_lost_packets(self):
-        return len(self.dropped_events) / (len(self.dropped_events) + len(self.events))
         return self.proportion_lost
 
+    # Wrapper to generate events, determines departure times, dropped packets, and calculates metrics.
     def run(self, num_packets, arrival_rate, observer_rate):
-        return self.generate_events(num_packets, arrival_rate, observer_rate)
+        self.generate_events(num_packets, arrival_rate, observer_rate)
 
+    # Generates events, determines departure times, dropped packets, and calculates metrics.
     def generate_events(self, arrival_rate, average_packet_length, observer_rate):
         self.generate_packet_events(arrival_rate, average_packet_length)
         self.simulate()
-        return self.generate_observer_events(observer_rate)
+        self.generate_observer_events(observer_rate)
 
+    # Generate arrival times for all packet events in the simulation. The dropped events
+    # and departure times are calculated separately, in the simulation function.
     def generate_packet_events(self, arrival_rate, average_packet_length):
         self.events = [Event(
             arrival_time=Utility.poisson(arrival_rate),  # length/arrival rate
@@ -41,6 +50,7 @@ class DiscreteEventSimulator:
         )]
         self.events[0].departure_time = self.events[0].arrival_time + self.events[0].service_time
         i = 1
+        # Stop when we have generated a packet with arrival time greater than or equal to the simulation time.
         while self.events[-1].arrival_time < self.simulation_time:
             prev_event = self.events[i - 1]
             new_event = Event(
@@ -51,6 +61,8 @@ class DiscreteEventSimulator:
             self.events.append(new_event)
             i += 1
 
+    # Generates all observer events based on the rate. We only care about the arrival time, since
+    # these are not packets.
     def generate_observer_events(self, observer_rate):
         self.observer_events = [Event(arrival_time=Utility.poisson(observer_rate))]
         i = 1
@@ -61,15 +73,14 @@ class DiscreteEventSimulator:
             )
             self.observer_events.append(new_event)
             i += 1
-        return self.get_observer_avg_num_packets_in_q()
+        self.calculate_metrics_using_observers()
 
-    def get_observer_avg_num_packets_in_q(self):
+    def calculate_metrics_using_observers(self):
         num_arrivals = 0
         num_departed = 0
         num_packets_lost = 0
         num_packets_passed = 0
-
-        packet_success_proportions = []
+        packet_success_proportions = 0
 
         all_events = self.get_sorted_events(True)
         local_q_idle_proportions = 0
@@ -77,34 +88,44 @@ class DiscreteEventSimulator:
         num_observers = len(self.observer_events)
 
         for event in all_events:
+            # Increment arrivals and passed packets
             if event[0] == 'Arrival':
                 num_arrivals += 1
                 num_packets_passed += 1
+            # Increment dropped count
             elif event[0] == 'Dropped':
-                # num_arrivals += 1
                 num_packets_lost += 1
+            # Increment departed count
             elif event[0] == 'Departure':
                 num_departed += 1
-            else:  # Observer
+            else:  # Observer, calculate all metrics necessary
                 packet_success_proportion = 0
                 num_packets_in_q = num_arrivals - num_departed
+                # Avoid divide by zero errors
                 if num_packets_passed > 0 or num_packets_lost > 0:
                     packet_success_proportion = num_packets_lost / (num_packets_passed + num_packets_lost)
-                packet_success_proportions.append(packet_success_proportion)
+                packet_success_proportions += packet_success_proportion
+                # capacity = 0 means the simulation is M/M/1
                 if self.capacity != 0:
                     num_packets_in_q = min(num_packets_in_q, self.capacity)
                 total_q_size += num_packets_in_q
                 local_q_idle_proportions += 1 if num_packets_in_q == 0 else 0
-                # total_q_size += num_packets_in_q
-        self.proportion_lost = sum(packet_success_proportions) / num_observers
+        # Set metrics to average based on number of observers
+        self.proportion_lost = packet_success_proportions / num_observers
         self.q_idle_proportions = local_q_idle_proportions / num_observers
-        return total_q_size / num_observers
+        self.avg_q_size = total_q_size / num_observers
 
     # Includes droppped events
     def get_all_sorted_events(self):
         events = self.dropped_events + self.events
         return sorted(events, key=lambda event: event.arrival_time)
 
+    # Used to calculate metrics.
+    # Map the event arrival_time to a tuple ("Arrival", arrival_time) for all events in self.events.
+    # Map the event departure_time to a tuple ("Departure", departure_time) for all events in self.events.
+    # Map the event arrival_time to a tuple ("Observer", arrival_time) for all events in self.observer_events,
+    # here arrival_time is arbitrary -- we just need a time stamp
+    # Map the event arrival_time to a tuple ("Dropped", arrival_time) for all events in self.dropped_events.
     def get_sorted_events(self, include_dropped=False):
         arrival_events = [('Arrival', event.arrival_time) for event in self.events]
         departure_events = [('Departure', event.departure_time) for event in self.events]
@@ -118,26 +139,35 @@ class DiscreteEventSimulator:
     # Set departure times and dropped_events
     def simulate(self):
         i = 1
+        # i can't iterate through events directly since, in the case of M/M/1/k simulations, elements of self.events
+        # are popped and moved to the "dropped_events" array.
         while i < len(self.events):
             prev_event = self.events[i - 1]
             cur_event = self.events[i]
+            # dequeue all events that have depart_time before cur_event's arrival_time
             self.move_queue(cur_event.arrival_time)
             cur_event.q_size = len(self.packet_queue)
+            # Enqueue if queue isn't empty or the last packet hasn't departed yet
             should_enqueue = prev_event.departure_time > cur_event.arrival_time or len(self.packet_queue) > 0
+            # If the capcity is not 0 (this represents infinite queue) and the queue size is greater or equal to the
+            # capcity, drop the event. Don't increment i, because self.events[i] is already the next event.
             if 0 < self.capacity <= len(self.packet_queue):
                 dropped_event = self.events.pop(i)
                 self.dropped_events.append(dropped_event)
                 # Don't increment so we don't skip any events
+            # If we should enqueue the event, but the queue isn't full, update its departure time based on the
+            # previous event's departure time. Add it to the queue.
             elif should_enqueue:
                 cur_event.was_enqueued = True
                 cur_event.departure_time = prev_event.departure_time + cur_event.service_time
                 self.packet_queue.append(cur_event.departure_time)
                 i += 1
+            # Queue is idle, just set the departure time to the arrival_time + service_time
             else:
                 cur_event.departure_time = cur_event.arrival_time + cur_event.service_time
                 i += 1
 
-    # dequeues all packets with departure_time < cur_time
+    # Dequeues all packets with departure_time < cur_time
     def move_queue(self, cur_time):
         self.packet_queue = deque(sorted(self.packet_queue))
         if len(self.packet_queue) == 0:
--- main.py
+++ main.py
@@ -3,33 +3,74 @@ from DiscreteEventSimulator import DiscreteEventSimulator
 import matplotlib.pyplot as plt
 
 def main():
-    run_MM1_simul()
-    # run_MM1k_simul()
+    # run_MM1_simul()
+    # run_MM1_large_util_simul()
+    run_MM1k_simul()
     # debug_MM1k_simul()
     # print(get_stats())
 
+# Compare M/M/1 simulation for rho=0.95 against rho-1.2 for different simulation times.
+def run_MM1_large_util_simul():
+    simul_times = [200, 400, 600, 800, 1000]
+    rhos = [0.95, 1.2]
+    transmission_rate = 1_000_000
+    avg_packet_length = 2000
+    for rho in rhos:
+        avg_packet_for_all_rhos = []
+        avg_packet_idle_for_all_rhos = []
+        for time in simul_times:
+            print(f"Time: {time}")
+            des = DiscreteEventSimulator(transmission_rate=transmission_rate, simulation_time=time)
+            arrival_rate = Utility.get_arrival_rate_from_rho(rho)
+            observer_rate = arrival_rate * 5
+            des.run(arrival_rate, avg_packet_length, observer_rate)
+            avg_q_size = des.avg_q_size
+            avg_packet_for_all_rhos.append(avg_q_size)
+            avg_packet_idle_for_all_rhos.append(des.q_idle_proportions)
 
+        plt.plot(simul_times, avg_packet_for_all_rhos)
+
+    # Plot logic
+    plt.title("Average packets in queue vs. simulation time:")
+    plt.xlabel("simulation time")
+    plt.ylabel("average number of packets in queue")
+    plt.legend(["rho = 0.95", "rho = 1.2"])
+    plt.ylim(bottom=0)
+    plt.show()
+
+    plt.figure(2)
+    plt.plot(simul_times, avg_packet_idle_for_all_rhos)
+    plt.title("Queue idle time vs. simulation time:")
+    plt.xlabel("simulation time")
+    plt.ylabel("proportion of time idle")
+    plt.ylim(bottom=0, top=1)
+    plt.legend()
+    plt.show()
+
+# Run M/M/1 simulation for different utilizations.
 def run_MM1_simul():
-    rhos = [0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95, 1.2]
+    rhos = [0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 0.85, 0.95]
     transmission_rate = 1_000_000
     avg_packet_length = 2000
     avg_packet_for_all_rhos = []
     avg_packet_idle_for_all_rhos = []
 
-    for i, rho in enumerate(rhos[:]):
+    for i, rho in enumerate(rhos):
         des = DiscreteEventSimulator(transmission_rate=transmission_rate, simulation_time=1000)
         print(f"Iteration {i}, rho: {rho}")
         arrival_rate = Utility.get_arrival_rate_from_rho(rho)
         observer_rate = arrival_rate * 5
-        avg_q_size = des.run(arrival_rate, avg_packet_length, observer_rate)
+        des.run(arrival_rate, avg_packet_length, observer_rate)
+        avg_q_size = des.avg_q_size
         avg_packet_for_all_rhos.append(avg_q_size)
         avg_packet_idle_for_all_rhos.append(des.q_idle_proportions)
 
+    # Plot logic
     plt.figure(1)
     plt.plot(rhos, avg_packet_for_all_rhos)
     plt.title("Average packets in queue vs. utilization:")
     plt.xlabel("utilization")
-    plt.ylabel("average number of packets lost")
+    plt.ylabel("average number of packets in queue")
     plt.show()
 
     plt.figure(2)
@@ -39,6 +80,7 @@ def run_MM1_simul():
     plt.ylabel("proportion of time idle")
     plt.show()
 
+# Run M/M/1/k simulation for different queue capacities and different utilizations.
 def run_MM1k_simul():
     Ks = [10, 25, 50]
     rhos = [float(i) / 10.0 for i in range(5, 15 + 1)]
@@ -52,17 +94,18 @@ def run_MM1k_simul():
         avg_packet_in_q_for_all_rhos = []
         avg_packet_loss_for_all_rhos = []
         for j, rho in enumerate(rhos):
-            des = DiscreteEventSimulator(capacity=k, transmission_rate=transmission_rate, simulation_time=1000)
+            des = DiscreteEventSimulator(capacity=k, transmission_rate=transmission_rate, simulation_time=20)
             arrival_rate = Utility.get_arrival_rate_from_rho(rho)
             print(f"Iteration {i}-{j}, k: {k}, rho: {rho}, lambda: {arrival_rate}")
             observer_rate = arrival_rate * 5
-            avg_q_size = des.run(arrival_rate, avg_packet_length, observer_rate)
-            avg_packet_in_q_for_all_rhos.append(avg_q_size)
+            des.run(arrival_rate, avg_packet_length, observer_rate)
+            avg_packet_in_q_for_all_rhos.append(des.avg_q_size)
             avg_packet_loss_for_all_rhos.append(des.proportion_of_lost_packets())
 
         avg_packets_in_q_for_all_ks.append(avg_packet_in_q_for_all_rhos)
         avg_packet_loss_for_all_ks.append(avg_packet_loss_for_all_rhos)
 
+    # Plot logic
     plt.figure(1)
     for i, k in enumerate(Ks):
         plt.plot(rhos, avg_packet_loss_for_all_ks[i], label=str(k))
@@ -74,9 +117,9 @@ def run_MM1k_simul():
     plt.figure(2)
     for i, k in enumerate(Ks):
         plt.plot(rhos, avg_packets_in_q_for_all_ks[i], label=str(k))
-    plt.title("Average packets in queue vs. utilization for all K values:")
+    plt.title("Average packets in queue vs. utilization for all queue sizes:")
     plt.xlabel("utilization")
-    plt.ylabel("average number of packets lost")
+    plt.ylabel("average number of packets in queue")
     plt.legend()
     plt.show()
 
